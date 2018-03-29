b1b195986bc9d55d00c86b15dd2f28fa89b148cb
Tero Teelahti <tero@armadainteractive.com>
2018-03-29 07:30:21 +0300

    Add Google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,18 @@ module.exports = {
         name: "markdown-pages",
       }
     },
-    'gatsby-transformer-remark'
+    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-52433903-2",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
   ]
 };
